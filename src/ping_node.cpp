cf9744c60ee984202bd7b90b8478833ed5980c1d
Christian Rauch <Rauch.Christian@gmx.de>
2017-04-24 23:06:23 +0100

    publish round-trip time

--- src/ping_node.cpp
+++ src/ping_node.cpp
@@ -1,10 +1,22 @@
 #include <ros/ros.h>
 #include <benchmark/ByteVectorStamped.h>
+#include <std_msgs/Float64.h>
 
-void pongCallback(const benchmark::ByteVectorStampedConstPtr &msg) {
-    const ros::Duration delay = ros::Time::now() - msg->time;
-    ROS_INFO_STREAM("RTT: " << delay.nsec << " ns");
-}
+class DelayPublischer {
+public:
+    DelayPublischer(const ros::Publisher &pub) : pub(pub) { }
+
+    void pongCallback(const benchmark::ByteVectorStampedConstPtr &msg) {
+        const ros::Duration delay = ros::Time::now() - msg->time;
+        ROS_INFO_STREAM("RTT: " << delay.nsec << " ns");
+        std_msgs::Float64 rtt;
+        rtt.data = delay.toSec();
+        pub.publish(rtt);
+    }
+
+private:
+    const ros::Publisher &pub;
+};
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "ping");
@@ -13,7 +25,11 @@ int main(int argc, char **argv) {
 
     ros::Publisher ping_pub = n.advertise<benchmark::ByteVectorStamped>("ping", 1);
 
-    ros::Subscriber pong_sub = n.subscribe("pong", 1, pongCallback);
+    ros::Publisher delay_pub = n.advertise<std_msgs::Float64>("roundtrip_time", 1);
+
+    DelayPublischer dpub(delay_pub);
+
+    ros::Subscriber pong_sub = n.subscribe("pong", 1, &DelayPublischer::pongCallback, &dpub);
 
     ros::Rate loop_rate(1);
 
@@ -23,7 +39,7 @@ int main(int argc, char **argv) {
         msg.time = ros::Time::now();
 
         ping_pub.publish(msg);
-        
+
         ros::spinOnce();
 
         loop_rate.sleep();
